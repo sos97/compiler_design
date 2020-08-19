b96750c91c0534f23034ab29e7800226e861e21d
sos97 <sardararitra97@gmail.com>
2020-08-19 13:01:08 +0000

    this is done using shell scripting..

--- first_file.cpp
+++ first_file.cpp
@@ -6,5 +6,6 @@ int main()
 {
  cout << "Hope that I will be able to make the compiler...God bless me!!" << endl;
  cout << "Actually I am learning all the functionality of git.." << endl;
+ cout << "This is the third experiment...;)" << endl;
  return 0;
 }
